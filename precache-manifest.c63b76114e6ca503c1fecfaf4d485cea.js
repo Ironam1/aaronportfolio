a257b32c3ce039c5ab27faab63a95800fcc5515f
Ironam1 <ironam1@icloud.com>
2019-11-25 10:37:55 -0500

    Updates

---
+++ precache-manifest.c63b76114e6ca503c1fecfaf4d485cea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "899686d7a9c5186d9538271c579825e3",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d38d0a47998f6d3d1e6b",
+    "url": "/portfolio/static/css/main.2a48309e.chunk.css"
+  },
+  {
+    "revision": "6c83305e3b3eb7428756",
+    "url": "/portfolio/static/js/2.b9171151.chunk.js"
+  },
+  {
+    "revision": "d38d0a47998f6d3d1e6b",
+    "url": "/portfolio/static/js/main.845d9c26.chunk.js"
+  },
+  {
+    "revision": "6b26cb3bc1b9147ba086",
+    "url": "/portfolio/static/js/runtime-main.86a05020.js"
+  }
+]);
